435efb440ad8b3bf0eb13851a95ce3f236bdad59
Jan Słowik <jas.slowik@gmail.com>
2022-01-29 16:14:50 +0100

    zip dvc

--- run_experiment_1.sh
+++ run_experiment_1.sh
@@ -1,65 +1,76 @@
 #!/bin/bash
 
 # opentopomap src rozne zoomy i modele opentopomap
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=CNN_SEP_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=CNN_AUG_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=12 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=13 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=14 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=opentopomap --zoom=15 --model-names=MLP
 
-# arcgis src rozne zoomy i modele arcgis
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=CNN_SEP_MLP
+# # arcgis src rozne zoomy i modele arcgis
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=CNN_SEP_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=CNN_AUG_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=12 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=13 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=14 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --zoom=15 --model-names=MLP
 
-# oba src rozne zoomy i modele
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=CNN_SEP_MLP
+# # oba src rozne zoomy i modele
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=CNN_SEP_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=CNN_SEP_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=CNN_AUG_MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=CNN_AUG_MLP
 
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --model-names=MLP
+# python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --model-names=MLP
 
 # rozne zoomy i modele
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=13 --model-names=CNN_SEP_MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=14 --model-names=CNN_SEP_MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --zoom=15 --model-names=CNN_SEP_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --zoom=12 -model-names=CNN_SEP_MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=15 -model-names=CNN_SEP_MLP
 
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=13 --model-names=CNN_AUG_MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=14 --model-names=CNN_AUG_MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --zoom=15 --model-names=CNN_AUG_MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --zoom=12 --model-names=CNN_AUG_MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=15 --model-names=CNN_AUG_MLP
 
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=13  --model-names=MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=14 --model-names=MLP
 python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=14 --zoom=15 --model-names=MLP
-python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=15 --zoom=12 --model-names=MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=15 --model-names=MLP
+
+#
+
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=14 --model-names=CNN_SEP_MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=15 --model-names=CNN_SEP_MLP
+
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=14 --model-names=CNN_AUG_MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=15 --model-names=CNN_AUG_MLP
+
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=12 --zoom=14 --model-names=MLP
+python huhuha/experiments/experiment_1.py run --epochs-list=10 --rep-num=5 --image-src=arcgis --image-src=opentopomap  --zoom=13 --zoom=15 --model-names=MLP
